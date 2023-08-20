88e625c812444f09c99c04c5a07c165ab159d5c5
Aneeka Mangal <aneekamangal@gmail.com>
2023-08-20 12:25:20 +0530

    added room page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Info from "./pages/Info";
 import "./App.css";
-
+import Room from "./pages/Room";
 
 function App() {
-  
   return (
     <div className="App">
       {/* Bringing You Closer, Virtually. */}
       <Routes>
         <Route path="/" element={<Info />} />
-       
+        <Route path="/room/:id" element={<Room />} />
       </Routes>
     </div>
   );
