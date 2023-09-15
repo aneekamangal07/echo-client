3430eb9c55a89796ab304549367253dbf0c1a9a4
Aneeka Mangal <aneekamangal@gmail.com>
2023-09-15 19:39:23 +0530

    created functions and peer service

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,6 +1,8 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useSocket } from "../context/SocketProvider";
 import ReactPlayer from "react-player";
+import peer from "../peer";
+
 const Room = () => {
   const socket = useSocket();
   const [remoteSocketId, setRemoteSocketId] = useState(null);
@@ -16,29 +18,58 @@ const Room = () => {
       audio: true,
       video: true,
     });
+    const offer = await peer.getOffer();
+    socket.emit("user:call", { to: remoteSocketId, offer });
     setMyStream(stream);
+  }, [remoteSocketId, socket]);
+
+  const handleIncomingCall = useCallback(
+    async ({ fromId, offer }) => {
+      setRemoteSocketId(fromId);
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
+      });
+      setMyStream(stream);
+      console.log(`incoming call `, fromId, offer);
+      const ansCall = await peer.answerCall(offer);
+      socket.emit("call:accepted", { to: fromId, ansCall });
+    },
+    [socket]
+  );
+
+  const handleCallAccepted = useCallback(({ fromId, ansCall }) => {
+    peer.setLocalDescription(ansCall);
+    console.log("Call Accepted!");
   }, []);
 
   useEffect(() => {
     socket.on("user:joined", handleUserJoined);
+    socket.on("incoming:call", handleIncomingCall);
+    socket.on("call:accepted", handleCallAccepted);
     return () => {
       socket.off("user:joined", handleUserJoined);
+      socket.off("incoming:call", handleIncomingCall);
+      socket.off("call:accepted", handleCallAccepted);
     };
-  }, [socket, handleUserJoined]);
+  }, [socket, handleUserJoined, handleIncomingCall, handleCallAccepted]);
+
   return (
     <div>
       <h1>room</h1>
       <p>{remoteSocketId ? "Connected" : "No one else is in the room"}</p>
       {remoteSocketId && <button onClick={handleCallUser}>call</button>}
-      <h1>my stream</h1>
       {myStream && (
-        <ReactPlayer
-          playing
-          muted
-          height="300px"
-          width="300px"
-          url={myStream}
-        />
+        <>
+          <h1>my stream</h1>
+          <ReactPlayer
+            playing
+            muted
+            height="300px"
+            width="300px"
+            url={myStream}
+          />
+        </>
       )}
     </div>
   );
---
+++ src/peer.js
@@ -0,0 +1,37 @@
+class PeerService {
+  constructor() {
+    if (!this.peer) {
+      this.peer = new RTCPeerConnection({
+        iceServers: [
+          {
+            urls: [
+              "stun:stun.l.google.com:19302",
+              "stun:global.stun.twilio.com:3478",
+            ],
+          },
+        ],
+      });
+    }
+  }
+  async answerCall(offer) {
+    if (this.peer) {
+      await this.peer.setRemoteDescription(offer);
+      const ansCall = await this.peer.createAnswer();
+      await this.peer.setLocalDescription(new RTCSessionDescription(ansCall));
+      return ansCall;
+    }
+  }
+  async setLocalDescription(ansCall) {
+    if (this.peer) {
+      await this.peer.setRemoteDescription(new RTCSessionDescription(ansCall));
+    }
+  }
+  async getOffer() {
+    if (this.peer) {
+      const offer = await this.peer.createOffer();
+      await this.peer.setLocalDescription(new RTCSessionDescription(offer));
+      return offer;
+    }
+  }
+}
+export default new PeerService();
